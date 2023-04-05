6e79f0e39e0452e3aca67c593ba8a004b4954d10
carlocek <carloceccherelli@libero.it>
2023-04-05 20:32:16 +0200

    optimized cuda version, added plot of its execution times

--- CUDA/main.cu
+++ CUDA/main.cu
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <cuda.h>
+#include <cuda_runtime.h>
 #include <stdio.h>
 #include <string>
 #include <cstring>
@@ -9,6 +10,7 @@
 #include <chrono>
 
 #define MIN(x,y) ((x) < (y) ? (x) : (y))
+#define MAX(x,y) ((x) < (y) ? (y) : (x))
 
 using namespace std;
 
@@ -31,44 +33,21 @@ __global__ void editDistKernel(char* devA, char* devB, int lenA, int lenB, unsig
 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 	int bd = blockDim.x;
 	int j = lenA+d-tid;
-	__shared__ extern unsigned int ds_currDiag[];
 
 	if(d < 1)
 	{
 		if(tid == 0)
-			ds_currDiag[0] = min(lenA+d, lenB+1);
+			d_currDiagPtr[0] = MIN(lenA+d, lenB+1);
 		else if(tid == lenA+d)
-			ds_currDiag[(lenA+d) % bd] = min(lenA+d, lenB+1);
+			d_currDiagPtr[lenA+d] = MIN(lenA+d, lenB+1);
 	}
-	if(tid < min(lenA+d, lenB+1) && tid > max(0, d-1))
+	if(tid < MIN(lenA+d, lenB+1) && tid > MAX(0, d-1))
 	{
 		if(devA[j-1] != devB[tid-1])
-			ds_currDiag[tid % bd] = 1 + MIN(d_prevDiagPtr[tid], MIN(d_prevDiagPtr[tid-1], d_prevprevDiagPtr[tid-1]));
+			d_currDiagPtr[tid] = 1 + MIN(d_prevDiagPtr[tid], MIN(d_prevDiagPtr[tid-1], d_prevprevDiagPtr[tid-1]));
 		else
-			ds_currDiag[tid % bd] = d_prevprevDiagPtr[tid-1];
+			d_currDiagPtr[tid] = d_prevprevDiagPtr[tid-1];
 	}
-//	__threadfence();
-	__syncthreads();
-	if(tid < lenA+1)
-	{
-		d_currDiagPtr[tid] = ds_currDiag[tid % bd];
-	}
-
-//	__threadfence();
-//	if(tid % blockDim.x == 0)
-//	{
-//		unsigned int* tmp = d_prevprevDiagPtr;
-//		d_prevprevDiagPtr = d_prevDiagPtr;
-//		d_prevDiagPtr = d_currDiagPtr;
-//		d_currDiagPtr = tmp;
-//	}
-//	__threadfence();
-//	if(tid == 0)
-//	{
-//		for(int i = 0; i < lenA+1; i++)
-//			printf("%d, ", d_currDiagPtr[i]);
-//		printf("\n");
-//	}
 }
 
 int main()
@@ -93,13 +72,6 @@ int main()
 	cudaMemcpy((void*)devA, (void*)A, (lenA+1)*sizeof(char), cudaMemcpyHostToDevice);
 	cudaMemcpy((void*)devB, (void*)B, (lenB+1)*sizeof(char), cudaMemcpyHostToDevice);
 
-	for(int i = 0; i < lenA; i++)
-		printf("%c", A[i]);
-	printf("\n");
-	for(int i = 0; i < lenB; i++)
-		printf("%c", B[i]);
-	printf("\n");
-
 	t1 = chrono::high_resolution_clock::now();
 
 	unsigned int* d_currDiag;
@@ -122,13 +94,12 @@ int main()
 	prevDiag[1] = 1;
 	cudaMemcpy((void*)d_prevprevDiag, (void*)prevprevDiag, (lenA+1)*sizeof(unsigned int), cudaMemcpyHostToDevice);
 	cudaMemcpy((void*)d_prevDiag, (void*)prevDiag, (lenA+1)*sizeof(unsigned int), cudaMemcpyHostToDevice);
-//	cudaMemcpy((void*)d_currDiag, (void*)currDiag, (lenA+1)*sizeof(unsigned int), cudaMemcpyHostToDevice);
 
 	d_prevprevDiagPtr = d_prevprevDiag;
 	d_prevDiagPtr = d_prevDiag;
 	d_currDiagPtr = d_currDiag;
 
-	int bDim = 256;
+	int bDim = 128;
 	int gDim = ceil((float)(lenA+1)/bDim);
 	int dmin = 2-lenA;
 	int dmax = lenB+1;
@@ -137,26 +108,23 @@ int main()
 	{
 //		int imax = min(lenA+d+1, lenB+1);
 //		int gDim = ceil((float)(imax)/bDim);
-		editDistKernel<<<gDim, bDim, (bDim)*sizeof(unsigned int)>>>(devA, devB, lenA, lenB, d_prevprevDiagPtr, d_prevDiagPtr, d_currDiagPtr, d);
-		cudaDeviceSynchronize();
+		editDistKernel<<<gDim, bDim>>>(devA, devB, lenA, lenB, d_prevprevDiagPtr, d_prevDiagPtr, d_currDiagPtr, d);
+//		cudaDeviceSynchronize();
 
-//		cudaMemcpy((void*)currDiag, (void*)d_currDiagPtr, (lenA+1)*sizeof(unsigned int), cudaMemcpyDeviceToHost);
-//		for(int i = 0; i < lenA+1; i++)
-//			printf("%d, ", currDiag[i]);
-//		printf("\n");
-//		printf("end of iteration\n");
-		if(d == dmax-1)
-		{
-			cudaMemcpy((void*)&ed, (void*)&d_currDiagPtr[lenA], 1*sizeof(unsigned int), cudaMemcpyDeviceToHost);
-		}
 		unsigned int* tmp = d_prevprevDiagPtr;
 		d_prevprevDiagPtr = d_prevDiagPtr;
 		d_prevDiagPtr = d_currDiagPtr;
 		d_currDiagPtr = tmp;
 	}
-//	cudaMemcpy((void*)currDiag, (void*)d_currDiagPtr, (lenA+1)*sizeof(unsigned int), cudaMemcpyDeviceToHost);
-
+	cudaDeviceSynchronize();
+	cudaMemcpy((void*)&ed, (void*)&d_prevDiag[lenA], 1*sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	
 	t2 = chrono::high_resolution_clock::now();
+	cudaFree((void*)devA);
+	cudaFree((void*)devB);
+	cudaFree(d_prevprevDiagPtr);
+	cudaFree(d_prevDiagPtr);
+	cudaFree(d_currDiagPtr);
 	time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
 	printf("\nelapsed time: %f\n", time_span.count());
 	printf("edit distance: %d\n", ed);
